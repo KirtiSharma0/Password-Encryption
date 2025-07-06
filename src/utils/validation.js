dc61ef44f3dd17f4baa859323385d6d5772268ac
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-06 20:06:11 +0530

    initial commit

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,7 +6,7 @@ const validatesignupdata = (req)=>{
   if(!firstName || !lastName)
     throw new Error("Name is not valid");
 else if (firstName.length<10 || firstName.length>25){
-    throw new Error("Firstname should be 10 to 25 chars");
+    throw new Error("Firstname should have length btw 10 to 25 chars");
 }
 else if(!validator.isEmail(email)){
     throw new Error ("email is not valid");
